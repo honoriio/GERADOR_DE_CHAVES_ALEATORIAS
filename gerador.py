2bdc00856fa31aca687b570ee2cd55e61dae93f2
Diego Honório <83922000+honoriio@users.noreply.github.com>
2023-12-12 09:26:26 -0300

    correções e atuaizações

--- gerador.py
+++ gerador.py
@@ -1,6 +1,40 @@
 import random
+# Tela de apresentação e escolhas de digitos.
+print('=' * 42)
+print(f'GERADOR DE CHAVES DE SEGURANÇA'.center(42))
+print('=' * 42)
+print(f'ESCOLHA QUANTOS DIGITOS DESEJA'.center(42))
+print(f'[1]  6 DIGITOS'.center(42))
+print(f'[2]  8 DIGITOS'.center(42))
+print(f'[3] 10 DIGITOS'.center(42))
+print(f'[4] 12 DIGITOS'.center(42))
+print(f'[5] 14 DIGITOS'.center(42))
+print(f'[6] 16 DIGITOS'.center(42))
+print(f'=' * 42)
+escolha = int(input('Informe o valor desejado: '))
+digito = 0
 
-dicionario =[
+#Condicional para atribuir o valor escolhido
+if 1 <= escolha <= 6:
+    if escolha == 1:
+        digito = 6
+    elif escolha == 2:
+        digito = 8
+    elif escolha == 3:
+        digito = 10
+    elif escolha == 4:
+        digito = 12
+    elif escolha == 5:
+        digito = 14
+    elif escolha == 6:
+        digito = 16
+else:
+    print('Escolha inválida, usando o padrão de 6 dígitos.')
+    digito = 6
+
+# Dicionario feito para gerar as chaves aleatorias
+
+dicionario = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
@@ -13,9 +47,12 @@ dicionario =[
     ',', '.', '<', '>', '/', '?'
 ]
 
-quantia = int(input('Informe a quantia que deseja?: '))
-caracteres = quantia
-chave = random.choices(dicionario, k=caracteres)
-
+chave = random.choices(dicionario, k=digito)
 resultado = ''.join(chave)
-print(resultado)
+
+print('A opção {} que contem {} Digitos'.format(escolha, digito))
+print(f'=' * 42)
+print('\033[31mCHAVE GERADA:\033[0m', end=' ')
+print('\033[32m', resultado, '\033[0m')
+print(f'=' * 42)
+print('PROGRAMA FINALIZADO')
