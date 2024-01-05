458a8a69b9c056b2a40be8eb1055fd3a1b2ae181
Diego Honório <83922000+honoriio@users.noreply.github.com>
2024-01-04 23:46:58 -0300

    add

--- teste.py
+++ teste.py
@@ -1,65 +1,16 @@
-import random
 
-# Tela de apresentação e escolhas de dígitos.
-print('=' * 42)
-print(f'GERADOR DE CHAVES DE SEGURANÇA'.center(42))
-print('=' * 42)
-print(f'ESCOLHA QUANTOS DÍGITOS DESEJA'.center(42))
-print(f'[1]  6 DÍGITOS'.center(42))
-print(f'[2]  8 DÍGITOS'.center(42))
-print(f'[3] 10 DÍGITOS'.center(42))
-print(f'[4] 12 DÍGITOS'.center(42))
-print(f'[5] 14 DÍGITOS'.center(42))
-print(f'[6] 16 DÍGITOS'.center(42))
-print(f'=' * 42)
-escolha = int(input('Informe o valor desejado: '))
-digito = 0
+from flask import Flask, render_template, request
 
-# Dicionario feito para gerar as chaves aleatórias
-dicionario = [
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y', 'Z',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-    'u', 'v', 'w', 'x', 'y', 'z',
-    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
-    '-', '_', '=', '+', '[', ']', '{', '}', ';', ':',
-    ',', '.', '<', '>', '/', '?'
-]
+app = Flask(__name__)
 
-# Condicional para atribuir o valor escolhido
-if 1 <= escolha <= 6:
-    if escolha == 1:
-        digito = 6
-    elif escolha == 2:
-        digito = 8
-    elif escolha == 3:
-        digito = 10
-    elif escolha == 4:
-        digito = 12
-    elif escolha == 5:
-        digito = 14
-    elif escolha == 6:
-        digito = 16
-else:
-    print('Escolha inválida, usando o padrão de 6 dígitos.')
-    digito = 6
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Ajusta a quantidade de números, letras e caracteres especiais com base no tamanho escolhido
-qtd_numeros = digito // 3
-qtd_letras = digito // 3
-qtd_caracteres = digito - qtd_numeros - qtd_letras
+@app.route('/enviar_dados', methods=['POST'])
+def enviar_dados():
+    nome = request.form.get('nome')
+    return render_template('resultado.html', nome=nome)
 
-# Gera a chave aleatória
-chave = random.choices(dicionario[:10], k=qtd_numeros) + random.choices(dicionario[10:62], k=qtd_letras) + random.choices(dicionario[62:], k=qtd_caracteres)
-random.shuffle(chave)
-resultado = ''.join(chave)
-
-print(f'A opção {escolha} que contém {digito} Dígitos')
-print(f'=' * 42)
-print('\033[31mCHAVE GERADA:\033[0m', end=' ')
-print('\033[32m', resultado, '\033[0m')
-print(f'=' * 42)
-print('PROGRAMA FINALIZADO')
+if __name__ == '__main__':
+    app.run(debug=True)
